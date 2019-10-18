672371f1ef2cd568ec19eb9eab7a58caca39ebdc
MuriithiGichuru <muriithigichuru@gmail.com>
2019-10-18 14:50:05 +0300

    2nd edit

--- configs/config.py
+++ configs/config.py
@@ -1,7 +1,7 @@
 class Development:
     # databasename://user:password@host:port/dbname
     # eg postgresql://postgres:123456@127.0.0.1:5432/petManagementSystem
-    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:1234@127.0.0.1:5432/inventory_system2'
+    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:1234@127.0.0.1:5432/inventory_system1'
     DEBUG = True
     SECRET_KEY = 'FSDJGLFDKJGLFKJG'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
